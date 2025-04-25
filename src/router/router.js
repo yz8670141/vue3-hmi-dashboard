6ef49252e18fe570a58e446f2b87d4c5285ff3e2
Brian1 <brina1@toimc.com>
2025-04-25 13:38:26 +0800

    設定github pages

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import CanvasView from '../views/CanvasView.vue'
-import StatusSimulate from '../views/StatusSimulate.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import CanvasView from "../views/CanvasView.vue";
+import StatusSimulate from "../views/StatusSimulate.vue";
 
 const routes = [
-  { path: '/', redirect: '/canvas' },
-  { path: '/canvas', component: CanvasView },
-  { path: '/simulate', component: StatusSimulate }
-]
+  { path: "/", redirect: "/canvas" },
+  { path: "/canvas", component: CanvasView },
+  { path: "/simulate", component: StatusSimulate },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHashHistory(""),
+  routes,
+});
 
-export default router
+export default router;
